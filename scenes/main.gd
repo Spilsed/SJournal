2c61bbb0c487cfe9c98180d703088b203021a189
Titan Spellos <titanspellos@gmail.com>
2024-01-09 19:53:13 -0500

    Added settings and browse
    Added the base for the settings menu and a full basic implementation of the browsing menu for viewing previous pages.

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,20 +1,49 @@
 extends Control
 
 @onready var title_label : Label = $VBoxContainer/Title
-@onready var page := preload("res://scenes/page.tscn")
+@onready var top_layer: CanvasLayer = $Top
+@onready var browse_container: VBoxContainer = $BrowseBackground/VBoxContainer/ScrollContainer/VBoxContainer
+@onready var browse_background: ColorRect = $BrowseBackground
+@onready var settings_background: ColorRect = $SettingsBackground
+
+@onready var page: Resource = preload("res://scenes/page.tscn")
+@onready var page_button: Resource = preload("res://scenes/support/page_button.tscn")
+
+func update_browse_menu():
+	# Delete all existing children
+	for child in browse_container.get_children():
+		child.queue_free()
+	
+	# Add all new children
+	var reverse_order: Array = range(len(Globals.journal_data))
+	reverse_order.reverse()
+	for i in reverse_order:
+		var page_button_inst: PageButton = page_button.instantiate()
+		browse_container.add_child(page_button_inst)
+		page_button_inst.setup(i, Globals.journal_data[i]["creation_date"], Globals.journal_data[i]["public_title"])
 
 func _on_new_page_pressed():
-	Globals.add_page()
-	var page_inst: Page = page.instantiate().set_script(Page.new())
-
+	var page_inst: Page = page.instantiate()
+	top_layer.add_child(page_inst)
+	page_inst.setup()
 
 func _on_search_pages_pressed():
-	pass # Replace with function body.
-
+	browse_background.show()
+	update_browse_menu()
 
 func _on_quit_pressed():
+	Globals.save_journal_data()
 	get_tree().quit()
 
+func _on_settings_pressed():
+	settings_background.show()
 
-func _on_hurry_pressed():
-	title_label.text = "PRONKED BOZO!"
+func _on_browse_back_pressed():
+	browse_background.hide()
+
+func _on_settings_back_pressed():
+	settings_background.hide()
+
+# Settings
+func _on_public_titles_toggled(toggled_on):
+	pass # Replace with function body.
